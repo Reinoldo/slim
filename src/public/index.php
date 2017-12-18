f64b24a31e3bbf785ddca86bcdfbf357c9837b55
Jhonatan Morais <jhonatanvinicius@gmail.com>
2017-12-18 08:28:25 -0200

    dia 3

--- src/public/index.php
+++ src/public/index.php
@@ -2,14 +2,30 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 require '../vendor/autoload.php';
 require '../config/config.php';
 
 
 $app = new \Slim\App(["settings" => $config]);
+
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
+    $this->logger->addInfo("vc acessou o /name");
+   
 
     return $response;
 });
