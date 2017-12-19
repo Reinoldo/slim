6020c525fe6a5ee2f52271f1bbd5556f262ee19e
Jhonatan Morais <jhonatanvinicius@gmail.com>
2017-12-19 08:43:12 -0200

    dia 4

--- src/public/index.php
+++ src/public/index.php
@@ -2,8 +2,6 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 require '../vendor/autoload.php';
 require '../config/config.php';
@@ -20,6 +18,44 @@ $container['logger'] = function($c) {
     return $logger;
 };
 
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$app->get('/', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Pagina Principal");
+    $this->logger->addInfo("vc acessou a pagina principal");
+   
+
+    return $response;
+});
+
+$app->get('/tickets', function (Request $request, Response $response) {
+    $this->logger->addInfo("Ticket list page");
+    //$mapper = new TicketMapper($this->db);
+    //$tickets = $mapper->getTickets();
+    $tickets = new stdClass();
+    $tickets->id = 1;
+    $tickets->nome = "jhonatan";
+    $response->getBody()->write(var_export($tickets, true));
+    return $response;
+});
+
+
+$app->get('/ticket/{id}', function (Request $request, Response $response, $args) {
+    $ticket_id = (int)$args['id'];
+    $mapper = new TicketMapper($this->db);
+    $ticket = $mapper->getTicketById($ticket_id);
+
+    $response->getBody()->write(var_export($ticket, true));
+    return $response;
+});
 
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
@@ -29,4 +65,7 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
 
     return $response;
 });
+
+
+
 $app->run();
